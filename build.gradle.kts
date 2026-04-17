a5f250e675a6dca37666feaa7fb7b2911f932542
victorlpgazolli <victorlpgazolli@gmail.com>
2026-04-17 11:25:33 -0300

    ci: fix build.gradle.kts structure and ensure direct -Xlinker flag

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,6 +23,7 @@ kotlin {
                 baseName = "barbatos"
                 val libgccPath = System.getenv("LIBGCC_PATH")
                 linkerOpts(
+                    "-Xlinker", "-mno-outline-atomics",
                     "-L/usr/lib/aarch64-linux-gnu",
                     "-L/usr/aarch64-linux-gnu/lib",
                     "--allow-shlib-undefined",
@@ -53,7 +54,6 @@ kotlin {
             }
         }
     }
-
 
     @OptIn(org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi::class)
     compilerOptions {
